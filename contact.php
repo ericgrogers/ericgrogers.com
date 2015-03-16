b5c4c5218296edb140cda19cd18136b2d9c60437
ericgrogers <ericgrogers@fullsail.edu>
2015-03-15 20:21:38 -0500

    fixing email

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php 
 $emailTo = 'ericgrogers@fullsail.edu';
-$siteTitle = 'Eric G Rogers';
+$siteTitle = 'developer@ericgrogers.com';
 
 error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
 
